e1923fcbab3ed90da683842a5952666b22ef62c5
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-01-09 18:29:06 +0300

    bump version → 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-gitlab-reporter",
-    version="0.2.0",
+    version="0.2.1",
     description="GitLab reporter with collapsable sections for Vedro framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_gitlab_reporter/__init__.py
+++ vedro_gitlab_reporter/__init__.py
@@ -1,4 +1,4 @@
 from ._gitlab_reporter import GitlabReporter
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ("GitlabReporter",)
