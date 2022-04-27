eec8371b546ae89e5620f4333bfc741d51960547
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-27 18:14:10 +0300

    fix tests

--- tests/test_gitlab_reporter.py
+++ tests/test_gitlab_reporter.py
@@ -5,12 +5,15 @@ from uuid import uuid4
 
 import pytest
 from baby_steps import given, then, when
+from rich.console import Console
 from rich.style import Style
 from vedro.core import Dispatcher
 from vedro.events import ArgParsedEvent, ScenarioFailedEvent, ScenarioRunEvent, StepFailedEvent
-from vedro.plugins.director import Reporter
+from vedro.plugins.director import DirectorPlugin, Reporter
 from vedro.plugins.director.rich.test_utils import (
+    chose_reporter,
     console_,
+    director,
     dispatcher,
     make_parsed_args,
     make_scenario_result,
@@ -19,12 +22,14 @@ from vedro.plugins.director.rich.test_utils import (
 
 from vedro_gitlab_reporter import GitlabReporter, GitlabReporterPlugin
 
-__all__ = ("dispatcher", "console_")
+__all__ = ("dispatcher", "console_", "director", "chose_reporter",)
 
 
 @pytest.fixture()
-def reporter(console_) -> GitlabReporterPlugin:
-    return GitlabReporterPlugin(GitlabReporter, console_factory=lambda: console_)
+def reporter(dispatcher: Dispatcher, console_: Console) -> GitlabReporterPlugin:
+    reporter = GitlabReporterPlugin(GitlabReporter, console_factory=lambda: console_)
+    reporter.subscribe(dispatcher)
+    return reporter
 
 
 @contextmanager
@@ -45,9 +50,10 @@ def test_gitlab_reporter():
 
 @pytest.mark.asyncio
 async def test_reporter_scenario_run_event(*, dispatcher: Dispatcher,
+                                           director: DirectorPlugin,
                                            reporter: GitlabReporterPlugin, console_: Mock):
     with given:
-        reporter.subscribe(dispatcher)
+        await chose_reporter(dispatcher, director, reporter)
 
         scenario_result = make_scenario_result()
         event = ScenarioRunEvent(scenario_result)
@@ -63,10 +69,11 @@ async def test_reporter_scenario_run_event(*, dispatcher: Dispatcher,
 
 @pytest.mark.asyncio
 async def test_reporter_scenario_failed_event_verbose0(*, dispatcher: Dispatcher,
+                                                       director: DirectorPlugin,
                                                        reporter: GitlabReporterPlugin,
                                                        console_: Mock):
     with given:
-        reporter.subscribe(dispatcher)
+        await chose_reporter(dispatcher, director, reporter)
         await dispatcher.fire(ArgParsedEvent(make_parsed_args(verbose=0)))
 
         scenario_result = make_scenario_result().mark_failed()
@@ -83,10 +90,11 @@ async def test_reporter_scenario_failed_event_verbose0(*, dispatcher: Dispatcher
 
 @pytest.mark.asyncio
 async def test_reporter_scenario_failed_event_verbose1(*, dispatcher: Dispatcher,
+                                                       director: DirectorPlugin,
                                                        reporter: GitlabReporterPlugin,
                                                        console_: Mock):
     with given:
-        reporter.subscribe(dispatcher)
+        await chose_reporter(dispatcher, director, reporter)
         await dispatcher.fire(ArgParsedEvent(make_parsed_args(verbose=1)))
 
         step_result = make_step_result().mark_failed().set_started_at(1.0).set_ended_at(3.0)
@@ -108,10 +116,11 @@ async def test_reporter_scenario_failed_event_verbose1(*, dispatcher: Dispatcher
 
 @pytest.mark.asyncio
 async def test_reporter_scenario_failed_event_verbose2(*, dispatcher: Dispatcher,
+                                                       director: DirectorPlugin,
                                                        reporter: GitlabReporterPlugin,
                                                        console_: Mock):
     with given:
-        reporter.subscribe(dispatcher)
+        await chose_reporter(dispatcher, director, reporter)
         await dispatcher.fire(ArgParsedEvent(make_parsed_args(verbose=2)))
 
         scenario_result = make_scenario_result()
