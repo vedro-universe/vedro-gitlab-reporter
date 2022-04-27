4ceff046163a5811a8f0f1ae445bc6e7c36b5989
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-27 19:49:36 +0300

    bump version → 1.0.1

--- vedro_gitlab_reporter/__init__.py
+++ vedro_gitlab_reporter/__init__.py
@@ -1,4 +1,4 @@
 from ._gitlab_reporter import GitlabCollapsableMode, GitlabReporter, GitlabReporterPlugin
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ("GitlabReporter", "GitlabReporterPlugin", "GitlabCollapsableMode",)
